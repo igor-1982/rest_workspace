ce55530ac1e8b231947fd866217b9e7a32976a96
Igor@xhpc1 <igor_zhangying@fudan.edu.cn>
2022-06-23 13:55:52 +0800

    IYZ

--- doc/examples/RIHF-NH3/ri3fn_from_pyscf.py
+++ doc/examples/RIHF-NH3/ri3fn_from_pyscf.py
@@ -4,7 +4,7 @@ import numpy
 from pyscf import gto, scf, ci,df,lib
 import scipy
 from time import ctime, time
-lib.num_threads(30)
+lib.num_threads(6)
 TimeStart = time()
 mol = gto.Mole(
         atom='''
@@ -12,9 +12,9 @@ mol = gto.Mole(
           H       -1.1788391019      1.8973746268      0.0000000000
           H       -2.5388353987      1.0925460144     -0.5263586446
           H       -2.5388400276      2.7556271745     -0.4338224694''',
-         charge=1,spin=1,basis='aug-cc-pv5z',verbose=4
+         charge=0,spin=0,basis='aug-cc-pv5z',verbose=4
       ).build()
-mf = scf.UHF(mol).density_fit(auxbasis="def2-tzvp-jkfit")
+mf = scf.RHF(mol).density_fit(auxbasis="def2-tzvp-jkfit")
 mf.init_guess = '1e'
 mf.diis = 'diis'
 print("Total energy:",mf.kernel())
