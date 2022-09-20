772c5a56e16a8bbbb83454d4c2d139c12f3375d2
jeanwsr <srwang20@fudan.edu.cn>
2022-09-20 16:34:09 +0800

    add stability example

---
+++ doc/examples/HF-stable/stab.py
@@ -0,0 +1,40 @@
+from pyscf import gto, scf, tools
+import numpy as np
+np.set_printoptions(suppress=True, precision=6, linewidth=200)
+
+mol = gto.M(atom='''H 0.0 0.0 0.0; H 0.0 0.0 2.0''', basis='cc-pvdz').build()
+mf = mol.RHF().set(chkfile="stab.chk").density_fit(auxbasis='def2-svp-jkfit').run()
+tools.dump_mat.dump_rec(mf.stdout, mf.mo_coeff, ncol=10)
+mf.verbose = 9
+mf.stability(external=False)
+
+exit()
+from pyscf import tdscf
+import scipy
+
+from pyscf.soscf import newton_ah
+g, hop, hdiag = newton_ah.gen_g_hop_rhf(mf, mf.mo_coeff, mf.mo_occ)
+x = np.zeros_like(g)
+x[0] = 1.0
+print(hop(x))
+
+A,B = tdscf.rhf.get_ab(mf)
+nov = A.shape[1]
+ApB = A.reshape(nov,nov)+B.reshape(nov,nov)
+print(A)
+print(B)
+print(ApB)
+e,v = scipy.linalg.eigh(ApB)
+print(e)
+#e,v = scipy.linalg.eigh(A.reshape(nov,nov)-B.reshape(nov,nov))
+#print(e)
+
+exit()
+mf.mo_coeff[:,1] *= -1
+mf.stability(external=False)
+A,B = tdscf.rhf.get_ab(mf)
+nov = A.shape[1]
+ApB = A.reshape(nov,nov)+B.reshape(nov,nov)
+print(ApB)
+e,v = scipy.linalg.eigh(ApB)
+print(e)
